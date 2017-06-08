19c206bc6d09aca1d68848cadf433ab163cad422
jackyip <sonyyip2001@gmail.com>
2017-06-07 20:53:28 -0400

    data cleaning

--- data_import.R
+++ data_import.R
@@ -41,3 +41,32 @@ write.csv(df15, file = "AMECO15.csv")
 write.csv(df16, file = "AMECO16.csv")
 write.csv(df17, file = "AMECO17.csv")
 write.csv(df18, file = "AMECO18.csv")
+
+# Rbind tables
+df_all <- rbind(df1,
+                df2,
+                df3,
+                df4,
+                df5,
+                df6,
+                df7,
+                df8,
+                df9,
+                df10,
+                df11,
+                df12,
+                df13,
+                df14,
+                df15,
+                df16,
+                df17,
+                df18)
+
+countries  = c('Austria', 'Belgium', 'Bulgaria','Cyprus','Croatia','Czech Republic','Denmark',
+               'Estonia' ,'Finland' ,'France' ,'Germany' ,'West Germany' ,
+               'Greece' ,'Hungary','Ireland' ,'Italy' ,'Latvia' ,'Lithuania','Luxembourg' 
+               ,'Malta' ,'Netherlands' ,'Portugal' ,'Poland' ,'Romania' ,'Slovenia', 
+               'Spain','Slovakia' ,'Sweden' ,'United Kingdom')
+
+features - c('Total population', 'Population: 0 to 14 years', 'Population: 15 to 64 years', 'Population: 65 years and over', 'Civilian employment, persons (national)', 'Unemployment rate: total :- Member States: definition EUROSTAT', 'Private final consumption expenditure at current prices', 'Price deflator private final consumption expenditure', 'Private final consumption expenditure at current prices per head of population', 'Collective consumption of general government at current prices', 'Total consumption at current prices', 'Gross fixed capital formation at current prices: total economy', 'Consumption of fixed capital at current prices: total economy', 'Gross capital formation at current prices: total economy', 'Gross national saving', 'Domestic demand excluding stocks at current prices', 'Final demand at current prices', 'Gross national income at current prices', 'Gross national income at current prices per head of population', 'Gross national disposable income', 'National disposable income', 'Gross domestic product at current prices', 'Gross domestic product at current prices per hour worked', 'Total annual hours worked: total economy', 'Domestic income at current prices', 'Compensation of employees: total economy', 'Taxes linked to imports and production: total economy', 'Subsidies: total economy', 'Gross operating surplus: total economy', 'Net operating surplus: total economy', 'Nominal compensation per employee: total economy', 'Net capital stock at 2010 prices: total economy', 'Net returns on net capital stock: total economy', 'Total factor productivity: total economy', 'Exports of goods and services at current prices (National accounts)', 'Imports of goods and services at current prices (National accounts)', 'Exports of goods at current prices (National accounts)', 'Exports of services at current prices (National accounts)', 'Imports of goods at current prices (National accounts)', 'Imports of services at current prices (National accounts)', 'Terms of trade goods and services (National accounts)', 'Net exports of goods and services at current prices (National accounts)', 'Net primary income from the rest of the world (National accounts)', 'Balance on current transactions with the rest of the world (National accounts)', 'Net lending (+) or net borrowing (-): total economy', 'Current account balance, Balance of payments statistics (BPM6) :- Partner: world', 'Total exports of goods :- Foreign trade statistics', 'Total imports of goods :- Foreign trade statistics', 'ECU-EUR exchange rates (annual averages) :- Units of national currency per EUR/ECU', 'Conversion rates between euro and former euro-zone national currencies', 'Nominal short-term interest rates', 'Nominal long-term interest rates', 'Yield curve', 'Current taxes on income and wealth: corporations', 'Gross disposable income, corporations', 'Net disposable income, corporations', 'Gross saving: corporations', 'Net saving: corporations', 'Net lending (+) or net borrowing (-): corporations', 'Net saving: households', 'Current tax burden: total economy :- ESA 2010', 'Net saving: general government :- ESA 2010', 'Net lending (+) or net borrowing (-): general government :- ESA 2010', 'General government consolidated gross debt :- Excessive deficit procedure (based on ESA 2010)')
+
